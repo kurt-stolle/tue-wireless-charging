41f8cfc69749e06857a8050b1a2e7f7afe8e0f46
RomarN <39047793+RomarN@users.noreply.github.com>
2018-05-14 09:32:12 +0200

    updated main loop

--- WirelessCharging/src/main.cpp
+++ WirelessCharging/src/main.cpp
@@ -17,17 +17,30 @@ int main(void) {
     // Start an infinite loop
     // One iteration of this loop can be viewed graphically in the provided flowchart
     volatile static int i = 0 ;
+
     while(1) {
+    	for(int a = 0; a < 10; a++){
+    	    	//send pulse
+    	    	//read sensor
+    	    	//store sensor value
+    	    	c->SendPulse();
+    	    	c->ReadSensor();
+    	    }
+    	    //calculate average sensor value
+    	    //calculate load
+    	    c->LoadCalculation();
+
     	// Measure the
     	// Detect a load
     	if (c->DetectLoad()){
     		c->Enable(); // Start charging if a load is found
-
-
+    		c->ReadSensor();//read sensor
+    		c->MPP();//maximum power point
+    		c->Enable();//adjust Inverter and boost converter
 
 
     	} else if (c->IsCharging()) {
-    		c->Disable(); // Stop charging is no load is found
+    		c->Disable(); // Stop charging if no load is found
     	}
 
     	// Increment counter (accesses memory)
