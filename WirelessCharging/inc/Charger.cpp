c5282ed7eab82ce85bf58d389a8a6f1e4104c789
RomarN <39047793+RomarN@users.noreply.github.com>
2018-06-06 16:23:09 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/kurt-stolle/tue-wireless-charging""
    This reverts commit c55d31ceabfcde7d1821ac74f520eab8a924f74a.

--- WirelessCharging/inc/Charger.cpp
+++ WirelessCharging/inc/Charger.cpp
@@ -5,117 +5,171 @@
  *      Author: kurts
  */
 
+<<<<<<< HEAD
 #include <Charger.h>
 #define 3V3 3.3
 static ADC_CLOCK_SETUP_T ADCSetup;
 static volatile uint8_t Burst_Mode_Flag = 0, Interrupt_Continue_Flag;
 
 
+=======
+#include "Charger.h"
+>>>>>>> 8df89b628b6838af94b316626df34bad6bbd7984
 
+// Constructor
 Charger::Charger() {
-	// GPDMA SETUP
-	Chip_GPDMA_Init(LPC_GPDMA);
+  // A/D CONVERTERS SETUP
+  Chip_ADC_Init(LPC_ADC, &ADCSetup);
+  Chip_ADC_SetSampleRate(LPC_ADC, &ADCSetup, ADCBitrate);
+  Chip_ADC_SetStartMode(LPC_ADC, ADC_NO_START, ADC_TRIGGERMODE_RISING); // Must be set for burst mode readings
 
-	// A/D CONVERTERS SETUP
-	Chip_ADC_Init(LPC_ADC, &ADCSetup);
-	Chip_ADC_SetSampleRate(LPC_ADC, &ADCSetup, ADC_BITRATE);
+  // PWM SETUP
+  Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_PWM1);            // Enable clock
+  PWM = ((pwm_t *) LPC_PWM1_BASE);                            // Cast region on memory to pwm_t pointer
+  PWM->TCR |= 1 << 0;                                         // Timer enable bit
+  PWM->TCR |= 1 << 3;                                         // PWM enable bit
+  PWM->MR0 = PWMCycleTime;                                    // PWM time to 100
+  PWM->LER = (1 << 0);                                        // Push new MR0 value
 
-	// PWM SETUP
-	Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_PWM1);                    // Enable clock
-	LPC_PWM1->TCR |= 1 << 0;                                            // Timer enable bit
-	LPC_PWM1->TCR |= 1 << 3;                                            // PWM enable bit
-	LPC_PWM1->MR0 = PWM_CYCLE_TIME;										// PWM time to 100
+  PWM->PCR = 0x0;                                             // All zeros
+  PWM->PCR |= (1 << 2) | (1 << 4) | (1 << 6);                 // Enable double-edged mode on PWM 2,4 and 6
+  PWM->PCR |= (1 << 10) | (1 << 12) | (1 << 14);              // Enable PWM 2,4 and 6
 
-	LPC_PWM1->PCR = 0x0;												// All zeros
-	LPC_PWM1->PCR |= (1 << 2) | (1 << 4) | (1 << 6);					// Enable double-edged mode on PWM 2,4 and 6
-	LPC_PWM1->PCR |= (1 << 10)| (1 << 12)| (1 << 14);					// Enable PWM 2,4 and 6
+  // CHARGING INDICATOR SETUP
+  Board_LED_Set(0, false);
+  charging = false;
 
-	// CHARGING INDICATOR SETUP
-	Board_LED_Set(0, false);
-	charging = false;
+  // POWER CALCULATION PIN INITIALIZATION
+  Chip_IOCON_PinMux(LPC_IOCON, 0, 24, IOCON_MODE_PULLUP, IOCON_FUNC1);  // Select pin P0.24 in AD0.1
+  Chip_ADC_EnableChannel(LPC_ADC, ADC_CH1, ENABLE);                    // Enable ADC channel 1
 
-	// POWER CALCULATION PIN INITIALIZATION
-	Chip_IOCON_PinMux(LPC_IOCON, 0, 25, IOCON_MODE_INACT, IOCON_FUNC1);	// Select pin P0.25 in AD0.2
-	Chip_ADC_EnableChannel(LPC_ADC, ADC_CH2, ENABLE);					// Enable ADC channel 2
+  Chip_IOCON_PinMux(LPC_IOCON, 0, 25, IOCON_MODE_PULLUP, IOCON_FUNC1);  // Select pin P0.25 in AD0.2
+  Chip_ADC_EnableChannel(LPC_ADC, ADC_CH2, ENABLE);                    // Enable ADC channel 2
 
-	Chip_IOCON_PinMux(LPC_IOCON, 0, 24, IOCON_MODE_INACT, IOCON_FUNC1);	// Select pin P0.24 in AD0.1
-	Chip_ADC_EnableChannel(LPC_ADC, ADC_CH1, ENABLE);					// Enable ADC channel 1
+  // LOAD DETECTION PIN INITIALIZATION
+  Chip_IOCON_PinMux(LPC_IOCON, 0, 23, IOCON_MODE_PULLUP, IOCON_FUNC1);  // Select pin P0.23 in AD0.0
+  Chip_ADC_EnableChannel(LPC_ADC, ADC_CH0, ENABLE);                    // Enable ADC channel 0
 
-	// LOAD DETECTION PIN INITIALIZATION
-	Chip_IOCON_PinMux(LPC_IOCON, 0, 23, IOCON_MODE_INACT, IOCON_FUNC1);	// Select pin P0.23 in AD0.0
-	Chip_ADC_EnableChannel(LPC_ADC, ADC_CH0, ENABLE);					// Enable ADC channel 0
+  // INVERTER PWM SIGNALS SETUP AND PIN INITIALIZATION
+  Chip_IOCON_PinMux(LPC_IOCON, 2, 1, IOCON_MODE_INACT, IOCON_FUNC1);  // Select pin P2.1 in PWM2 mode
+  Chip_IOCON_PinMux(LPC_IOCON, 2, 3, IOCON_MODE_INACT, IOCON_FUNC1);  // Select pin P2.3 in PWM4 mode
+  SetInverterDutyCycle(0.4f);
 
-	// INVERTER PWM SIGNALS SETUP AND PIN INITIALIZATION
-	Chip_IOCON_PinMux(LPC_IOCON, 2, 1, IOCON_MODE_INACT, IOCON_FUNC1);  // Select pin P2.1 in PWM2 mode
-	Chip_IOCON_PinMux(LPC_IOCON, 2, 3, IOCON_MODE_INACT, IOCON_FUNC1);  // Select pin P2.3 in PWM4 mode
-
-	LPC_PWM1->MR1 = 0; 												// PWM2 set at 0
-	LPC_PWM1->MR2 = PWM_CYCLE_TIME * 0.4; 							// PWM2 reset
-	LPC_PWM1->MR3 = PWM_CYCLE_TIME * 0.4;							// PWM4 set
-	LPC_PWM1->MR4 = PWM_CYCLE_TIME;									// PWM4 reset at T-1
-	LPC_PWM1->LER = (1<<0) | (1<<1) | (1<<2) | (1<<3) | (1<<4); 	// Push new MR0-4 values
-
-	// BOOST CONVERTER PWM CONTROL
-	Chip_IOCON_PinMux(LPC_IOCON, 2, 5, IOCON_MODE_INACT, IOCON_FUNC1); 	// Select pin P2.5 in PWM6 mode
+  // BOOST CONVERTER PWM CONTROL
+  Chip_IOCON_PinMux(LPC_IOCON, 2, 5, IOCON_MODE_INACT, IOCON_FUNC1);    // Select pin P2.5 in PWM6 mode
 }
 
-// Enable starts the charger
-void Charger::Enable() {
-	// Set the charging flag
-	charging = true;
+// StopCharging starts the charger
+void Charger::StopCharging() {
+  // Set the charging flag
+  charging = false;
 
-	// Enable the LED indicator
-	Board_LED_Set(0, true);
+  // Set the duty cycle of the PWM going to the boost converter to 0
+  SetBoostConverterDutyCycle(0.0f);
+
+  // Enable the LED indicator
+  Board_LED_Set(0, true);
 }
 
-// Disable stops the charger
-void Charger::Disable() {
-	// Disable charging flag
-	charging = false;
+// StartCharging stops the charger
+void Charger::StartCharging() {
+  // Set the charging flag
+  charging = true;
 
-	// Disable LED indicator
-	Board_LED_Set(0, false);
+  // Set the duty cycle of the PWM going to the boost converter to 0.5
+  SetBoostConverterDutyCycle(0.5f);
+
+  // Disable LED indicator
+  Board_LED_Set(0, false);
 }
 
 double Charger::GetPower() {
-	uint16_t dataVoltage;
-	uint16_t dataCurrent;
+  if (!IsCharging()) {
+	return 0;
+  }
 
+  // Allocate variables to read into
+  uint16_t temp;
+  uint16_t dataCurrent = 0;
+  uint16_t dataVoltage = 0;
+
+  // Enable burst mode - start conversions
+  Chip_ADC_SetBurstCmd(LPC_ADC, ENABLE);
+
+  // Wait for A/D conversion to complete on CH2
+  for (uint16_t i = 0; i < powerMeasurementAverages; i++){
+    while (Chip_ADC_ReadStatus(LPC_ADC, ADC_CH2, ADC_DR_DONE_STAT) != SET) {}
+
+<<<<<<< HEAD
 	// Enable burst mode
 	Chip_ADC_SetBurstCmd(LPC_ADC, ENABLE);
 
 	 // Wait for A/D conversion to complete
 	while (Chip_ADC_ReadStatus(LPC_ADC, ADC_CH2, ADC_DR_DONE_STAT) != SET) {}
+=======
+    // Read the value of the ADC on CH2
+    Chip_ADC_ReadValue(LPC_ADC, ADC_CH2, &temp);
+    dataCurrent += temp;
 
-	// Read the value of the ADC on CH2 into dataCurrent
-	Chip_ADC_ReadValue(LPC_ADC, ADC_CH2, &dataCurrent);
+>>>>>>> 8df89b628b6838af94b316626df34bad6bbd7984
 
-    // Wait for A/D conversion to complete
-	while (Chip_ADC_ReadStatus(LPC_ADC, ADC_CH1, ADC_DR_DONE_STAT) != SET) {}
+    // Wait for A/D conversion to complete on CH1
+    while (Chip_ADC_ReadStatus(LPC_ADC, ADC_CH1, ADC_DR_DONE_STAT) != SET) {}
 
-	// Read the value of the ADC on CH1 into dataVoltage
-	Chip_ADC_ReadValue(LPC_ADC, ADC_CH1, &dataVoltage);
+    // Read the value of the ADC on CH1
+    Chip_ADC_ReadValue(LPC_ADC, ADC_CH1, &temp);
+    dataVoltage += temp;
+  }
 
-	// Disable burst mode
-	Chip_ADC_SetBurstCmd(LPC_ADC, DISABLE);
+  // Disable burst mode - stop reading
+  Chip_ADC_SetBurstCmd(LPC_ADC, DISABLE);
 
+  // Take the average value
+  dataCurrent/=powerMeasurementAverages;
+  dataVoltage/=powerMeasurementAverages;
+
+<<<<<<< HEAD
     // Parse sensor readings
 	double V = (dataVoltage*3V3)/(4096);
 	double I = (dataCurrent*3V3)/(4096);
+=======
+  // Parse sensor readings: 3V3 * data/2^12
+  double V = (dataVoltage * 3.3) / 4096.0;
+  V *= 2; // Scale of actual voltage level
+>>>>>>> 8df89b628b6838af94b316626df34bad6bbd7984
 
-    // Return power
-	return V*I;
+  double I = (dataCurrent * 3.3) / 4096.0;
+  I -= 2.5; // Offset of 2.5
+  I /= 0.04; // 40mV/A
+
+  // Return power
+  return V * I;
 }
 
 // IsCharging indicates whether charging has started
 bool Charger::IsCharging() {
-	return charging;
+  return charging;
 }
 
-
 // DetectLoad will send out a pulse and measure the response of the sensor
-bool Charger::DetectLoad() {
+bool Charger::IsLoadPresent() {
 
-	return true;
+  return true;
+}
+
+// SetInverterDutyCycle sets the duty cycle of the double PWM signal that should be wired to the inverter
+void Charger::SetInverterDutyCycle(float ratio) {
+  PWM->MR1 = 0;                                    // PWM2 set at 0
+  PWM->MR2 = (uint32_t) (PWMCycleTime * ratio);    // PWM2 reset
+  PWM->MR3 = (uint32_t) (PWMCycleTime * ratio);    // PWM4 set
+  PWM->MR4 = PWMCycleTime;                        // PWM4 reset at T-1
+  PWM->LER = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);    // Push new MR1-4 values
+}
+
+// SetBoostConverterDutyCycle sets the duty cycle of the PWM signal that should be wired to the boost converter
+void Charger::SetBoostConverterDutyCycle(float ratio) {
+  PWM->MR5 = 0;                                    // PWM6 set at 0
+  PWM->MR6 = (uint32_t) (PWMCycleTime * ratio);    // PWM6 reset
+  PWM->LER = (1 << 0) | (1 << 5) | (1 << 6);            // Push new MR5-6 values
 }
 
